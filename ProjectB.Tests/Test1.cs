224b6bdd3017e5996557f8e9417de97d8ab4d957
DeviN1111111 <1100961@hr.nl>
2025-05-23 16:49:01 +0200

    WE'RE SO BACK V3
    unit test finally work GG
    
    Co-Authored-By: Peter <193795330+AquaC2@users.noreply.github.com>
    Co-Authored-By: Chengie14141 <199114801+Chengie14141@users.noreply.github.com>
    Co-Authored-By: Lucior <183356814+EyeMustache@users.noreply.github.com>

--- ProjectB.Tests/Test1.cs
+++ ProjectB.Tests/Test1.cs
@@ -1,10 +1,49 @@
-﻿namespace ProjectB.Tests;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+namespace ProjectB;
 
 [TestClass]
-public sealed class Test1
+[DoNotParallelize]
+public class SessionManagerTests
 {
-    [TestMethod]
-    public void TestMethod1()
+    [TestInitialize]
+    public void Setup()
     {
+        SessionManager.Logout();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        SessionManager.Logout();
+    }
+
+    [DataTestMethod]
+    [DataRow("Cheng", true)]
+    [DataRow("Mario", true)]
+    [DataRow("Mario1", true)]
+    [DataRow("Mario2", true)]
+    [DataRow("Mario3", true)]
+    [DataRow("Mario4", true)]
+    [DataRow("Mario5", true)]
+    [DataRow("Mario6", true)]
+    [DataRow("Mario7", true)]
+    [DataRow("Mario8", true)]
+    [DataRow("Mario9", true)]
+    [DataRow("Mario10", true)]
+    [DataRow("Mario11", true)]
+    
+    public void SetCurrentUser_ValidUsernames_SetsCorrectUsernameAndLoginState(string firstname, bool expectedLoginState)
+    {
+        // Arrange
+        var user = new User { FirstName = firstname };
+
+        // Act
+        SessionManager.SetCurrentUser(user);
+
+        // Assert
+        Assert.AreEqual(firstname, SessionManager.CurrentUser.FirstName);
+        Assert.AreEqual(expectedLoginState, SessionManager.IsLoggedIn());
+        Assert.IsTrue((DateTime.Now - SessionManager.LoginTime).TotalSeconds < 1);
     }
 }
